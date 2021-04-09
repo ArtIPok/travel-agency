2311e86d4739afa9b235295a4bd1eb6f2b56a8f2
ArtIPok <apokora@itsap.pl>
2021-04-09 11:18:40 +0200

    Add unit testing ver4

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -12,6 +12,7 @@ describe('Component TripSummary', () => {
 
   it('should have img with correct src and alt', () => {
     const props = {
+      id: 'abc',
       image: '/abc/d.jpg',
       name: 'test image',
     };
@@ -24,6 +25,7 @@ describe('Component TripSummary', () => {
 
   it('should render correctly props: name, cost and days', () => {
     const props = {
+      id: 'abc',
       name: 'Test Trip',
       days: 2,
       cost: '123$',
@@ -35,32 +37,26 @@ describe('Component TripSummary', () => {
     expect(component.find('.details').find('span').at(1).text()).toEqual('from 123$');
   });
 
-  it('lack any props with crashing', () => {
-    const props = {
-      id: 'abc',
-      image: 'test',
-      name: 'test',
-      cost: '123$',
-      days: 2,
-    };
+  it('lack of any props with crashing', () => {
 
-    const component = shallow(<TripSummary {...props} />);
+    const component = shallow(<TripSummary />);
     expect(component).toBeTruthy();
   });
 
   it('should render in correct order in span for 3 tags', () => {
     const tags = ['tag1', 'tag2', 'tag3'];
+    const id = 'abc';
 
-    const component = shallow(<TripSummary tags={tags} />);
+    const component = shallow(<TripSummary tags={tags} id={id} />);
     expect(component.find('.tags').find('span').at(0).text()).toEqual('tag1');
     expect(component.find('.tags').find('span').at(1).text()).toEqual('tag2');
     expect(component.find('.tags').find('span').at(2).text()).toEqual('tag3');
   });
 
   it('should render div with class tags only when tags is not false or empty', () => {
-    const tags = [tags];
+    const id = 'abc';
 
-    const component = shallow(<TripSummary tags={tags} />);
+    const component = shallow(<TripSummary tags id={id} />);
     expect(component).toBeTruthy();
   });
 });
