a49712ec54d72b0a88e2eb2103c9798151c21fa8
ArtIPok <apokora@itsap.pl>
2021-04-09 11:57:07 +0200

    Add send to API ver1

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,6 +5,37 @@ import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import Pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { settings } from '../../../data/settings';
+import Button from '../../common/Button';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, options, setOrderOption}) => (
   <Grid>
@@ -25,6 +56,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
       <Col xs = {12}>
         <OrderSummary costOrder={tripCost} options={options}/>
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>;
     </Row>
   </Grid>
 );
